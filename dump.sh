78c12e7749c07f07fc03771746cd6efee32dee84
laygond <laygond_bryan@hotmail.com>
2020-08-08 21:14:22 -0400

    :sparkles: transfer tools created

--- dump.sh
+++ dump.sh
@@ -1,14 +1,24 @@
+# Dumps given directory into this shell file directory 
+# By Bryan Laygond @laygond
+
 if [[ $# -eq 2 ]]
 then
     # Get Location of this running shell file
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-    
-    if [[ -d $1 ]]
+    echo "[INFO] This shell file is running at $DIR"
+
+    if [[ -d $1 && ! -d $2  ]]
     #if source directory exist and 'new name folder' is not repeated 
     then
-        cp -r "$1/*" $DIR/$2
+        cp -r $1 $DIR/$2
+        echo "[INFO] Transfer complete"
+    else
+        echo "[ERROR] Error in arguments given"
+        echo "        First argument  : location of source folder"
+        echo "        Second argument : new name of source folder" 
     fi    
 else
-    echo "First argument  : location of source folder"
-    echo "Second argument : new name of source folder" 
+    echo "[ERROR] Error in arguments given"
+    echo "        First argument  : location of source folder"
+    echo "        Second argument : new name of source folder" 
 fi
